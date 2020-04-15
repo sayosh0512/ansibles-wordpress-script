95796e0b3d1de46ebaba1e4dc63d35b8943133ab
sayosh0512 <63592135+sayosh0512@users.noreply.github.com>
2020-04-15 11:57:50 +0530

    Update setup_ansible.sh

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
-local ip=$1
+
+echo "Public Ip is : ${1}" >> /tmp/var.txt
 ssh_key_configuration() {
-local pip = $1
+
 azurepass=P@ssw0rd@1202
-ssh-keygen
-ssh-copy-id azureadmin@pip password azurepass
+ssh-keygen /home/azureadmin/.ssh/id_rsa \n \n 
+ssh-copy-id azureadmin@${1} password azurepass yes\n
 #40.117.99.34
 }
 
 install_ansible() {
+echo "Public Ip is : ${1}" >> /tmp/var.txt
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
+
 }
 
 configure_ansible() {
-local ctrlpubip= $1
+echo "Configure ansible Ip is : ${1}" >> /tmp/var.txt
+#local ctrlpubip= $1
 #local ctrlpubip=192.168.0.1
 sudo chmod 777 /etc/ansible/hosts
-sudo echo -e "[webservers]\n${ctrlpubip}" >>/etc/ansible/hosts
+sudo echo -e "[webservers]\n${1}" >>/etc/ansible/hosts
 }
 
 wordpress_install() {
 cd /home/azureadmin
 sudo git clone https://github.com/sayosh0512/wordpressplaybook.git
-ansible-playbook playbook.yml -i /etc/ansible/hosts -u azureadmin
+ansible-playbook /home/azureadmin/wordpressplaybook/playbook.yml -i /etc/ansible/hosts -u azureadmin
 
 }
 
-ssh_key_configuration ip >> /tmp/var.txt
+
+ssh_key_configuration >> /tmp/var.txt
+install_ansible ${1} >> /tmp/var.txt 
+configure_ansible ${1} >> /tmp/var.txt
 wordpress_install >> /tmp/var.txt
-install_ansible >> /tmp/var.txt
-configure_ansible ip >> /tmp/var.txt
+
